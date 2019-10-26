3dd72ce38219fce09acf5dd11bbe1d70c54a91ff
John Lewis <johnlewis@MacBook-Pro.local>
2019-10-26 13:05:03 -0600

    third

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Playerinfo from './components/Playerinfo'
+import Team from './components/Team'
+import {Switch, Route} from 'react-router-dom'
+
+export default (
+    <Switch>
+        <Route exact path='/' component = {Home}/>
+        <Route path='/team' component = {Team}/>
+        <Route path='/Playerinfo' component = {Playerinfo}/>
+    </Switch>
+)
