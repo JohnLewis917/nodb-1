03a748481fb476037a25a6bdddbc383af071f2a7
John Lewis <johnlewis@MacBook-Pro.local>
2019-10-26 13:05:23 -0600

    third

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,2 +1,23 @@
 import React, {Component} from 'react'
-import 
+import Playerinfo from './Playerinfo'
+import axios from 'axios'
+
+export default class Team extends Component {
+    constructor() {
+        super()
+        this.state = {
+            allPlayers: []
+        }
+    }
+    componentDidMount(){
+        axios.get('/api/playerinfo')
+        .then(res => {
+            this.setState({
+                allPlayers: res.data
+            })
+        })
+    }
+    updatePlayers(id, body){
+        axios.put
+    }
+}
