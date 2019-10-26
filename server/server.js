9f13fe520e3313848af1b82899777242a8560571
John Lewis <johnlewis@MacBook-Pro.local>
2019-10-25 18:55:19 -0600

    second

---
+++ server/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const {SERVER_PORT} =process.env
+const ctrl = require('./controllers/controller')
+
+const app = express()
+
+
+app.use(express.json())
+
+app.post('/api/home', ctrl.addPlayers)
+app.get('/api/home', ctrl.getPlayers)
+app.put('/api/home/:id', ctrl.updatePlayers)
+app.delete('api/home/:id', ctrl.deletePlayers)
+
+app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT} A Space Odessey`))
