4297cee8296d68f58a2a0651c145c232cf3aef22
John Lewis <johnlewis@MacBook-Pro.local>
2019-10-26 23:53:48 -0600

    changes 10-26

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React, {Component}from 'react'
+
+export default class Home extends Component {
+    constructor(){
+        super()
+
+        
+    }
+    render(){
+        return(
+            <div>
+                <text>
+                    Hello World
+                </text>
+            </div>
+        )
+    }
+}
