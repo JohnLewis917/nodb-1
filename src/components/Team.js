6f66b3764ba5b7cfb66e96ed4a8c8414adab6691
John Lewis <johnlewis@MacBook-Pro.local>
2019-10-26 23:54:15 -0600

    changes 10-26

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import Playerinfo from './Playerinfo'
+import Change from './Change'
 import axios from 'axios'
 
 export default class Team extends Component {
@@ -18,6 +19,34 @@ export default class Team extends Component {
         })
     }
     updatePlayers(id, body){
-        axios.put
+        axios.put(`/api/change/${id}`, body)
+        .then(res => {
+            this.setState({
+                allPlayers: res.data
+            })
+        })
+    }
+    deletePlayer(id){
+        axios.delete(`/api/change/${id}`)
+        .then(res => {
+            this.setState({
+                allPlayers: res.data 
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div className="team">
+                
+                {this.state.allPlayers.map(el => (
+                <Change changeObj={el} key={el.id}
+                updatePlayerFn={this.updatePlayer}
+                deletePlayerFn={this.deletePlayer}/>
+                ))}
+                <button onClick={() => this.editPlayer()}>Edit</button>
+                <button onClick={() => this.props.deletePlayer(this.props.pokemonObj.id)}>Delete</button>
+            </div>
+        )
     }
 }
