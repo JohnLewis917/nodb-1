0bd1be666d1698f40faaa34edcbb1ec137e27413
John Lewis <johnlewis@MacBook-Pro.local>
2019-10-27 16:09:34 -0600

    changes 10-27

--- src/components/Playerinfo.js
+++ src/components/Playerinfo.js
@@ -15,42 +15,38 @@ export default class Playerinfo extends Component {
     };
   }
   submitPlayer() {
-    axios.post("/api/allPlayers", this.state)
-    .then(res => {
-        this.props.history.push('/team')
+    axios.post("/api/players", this.state)
+    .then(res => {console.log (res)
+        this.props.history.push('/')
     });
   }
   render() {
       return (
           <div className = "playerinfo-outer">
-          {/* <img className = "image"
-            src={`https://robohash.org/player.png`}
-            width="125"
-            height="125"
-            alt=""
-          /> */}
-        <form>
+            <img className="image-2" src="http://www.clker.com/cliparts/e/0/6/3/12550941262092997998Olympic_sports_Athletics_pictogram.svg.hi.png" alt="run"/>
+            <img className="image-3" src="http://www.clipartpanda.com/clipart_images/sport-clip-art-vector-clip-5496335/download" alt="bb"/>
+        <form className= "text-box" action="/controllers/controller">
           
           <p>
-            <input type="text" name="firstname" placeholder="First Name"/>
+            <input className= "input" type="text" name="firstname" placeholder="First Name"/>
           </p>
           <p>
-            <input type="text" name="lastname" placeholder="Last name"/>
+            <input className= "input" type="text" name="lastname" placeholder="Last name"/>
           </p>
           <p>
-            <input type="number" name="age" placeholder="Age"/>
+            <input className= "input" type="number" name="age" placeholder="Age"/>
           </p>
           <p>
-            <input type="text" name="gender" placeholder="Gender" /> 
+            <input className= "input" type="text" name="gender" placeholder="Gender" /> 
           </p>
           <p>
-            <input type="text" name="city" placeholder="City"/>
+            <input className= "input" type="text" name="city" placeholder="City"/>
           </p>
           <p>
-            <input type="text" name="state" placeholder="State"/>
+            <input className= "input" type="text" name="state" placeholder="State"/>
           </p>
           <p>
-            <input type="text" name="number" placeholder="###-###-####"/>
+            <input className= "input" type="text" name="number" placeholder="###-###-####"/>
           </p>
           <input onClick={() => this.submitPlayer()} type="submit" value="Submit" />
         </form>
