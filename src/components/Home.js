5e25ae1d42f044928c5a90492d52bd8aee3fa4a6
John Lewis <johnlewis@MacBook-Pro.local>
2019-10-27 22:27:36 -0600

    new changes 10-27

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, {Component}from 'react'
+import React,{Component}from 'react'
 import { Redirect } from 'react-router-dom'
 
 export default class Home extends Component {
@@ -26,6 +26,7 @@ export default class Home extends Component {
                 <p className = "text">
                     Team Builder allows you to automatically build a sports team just by entering the players information. You can also add an image of the player. All information can be edited and players can be deleted if needed. 
                 </p>
+                {this.renderRedirect()}
                 <button onClick={this.setRedirect}className="home-button">Build A Team!</button>
                 
             </div>
